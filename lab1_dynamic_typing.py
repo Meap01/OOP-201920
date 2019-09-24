8692f06343f3eb353a6f8ac429e26758752432b5
Daniel Krasovski <dkrasovskijj@gmail.com>
2019-09-24 15:38:57 +0100

    Update lab1_dynamic_typing.py

--- lab1_dynamic_typing.py
+++ lab1_dynamic_typing.py
@@ -7,11 +7,15 @@
 my_variable = 10
 while my_variable < 20:
         i = my_variable
+        my_variable += 1
         if i < 12:
-                my_variable +=1
+
                 print(my_variable)
+
+
         else:
                 my_vairable = (my_variable + 20)
                 print('new')
+                break
 
 
